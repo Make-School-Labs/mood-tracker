19699ae1bcb92e94050238702df3f3cac087f080
LinnierGames <esericksanc@gmail.com>
2018-08-09 10:57:03 -0700

    init commit

---
+++ Mood Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mood Tracker
+//
+//  Created by Erick Sanchez on 8/9/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
