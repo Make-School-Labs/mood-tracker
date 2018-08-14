ea9457d1b4e08e0c79fcaa154a7817093bc1981d
LinnierGames <esericksanc@gmail.com>
2018-08-14 12:31:45 -0700

    Conforming the `ViewController` to the `UITableViewDataSource` methods

--- Mood Tracker/ViewController.swift
+++ Mood Tracker/ViewController.swift
@@ -14,3 +14,20 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mood entry cell", for: indexPath)
+        
+        let entry = entries[indexPath.row]
+        cell.textLabel?.text = entry.mood.stringValue
+        cell.detailTextLabel?.text = String(describing: entry.date)
+        
+        return cell
+    }
+}
+
