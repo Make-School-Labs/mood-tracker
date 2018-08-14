a6a8ec1d59fd5008f062a11b1d3c5fcb73b365e4
LinnierGames <esericksanc@gmail.com>
2018-08-14 13:59:56 -0700

    Connect the Storyboard with the DetailedEntryViewController

--- Mood Tracker/MoodDetailedViewController.swift
+++ Mood Tracker/MoodDetailedViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class MoodDetailedViewController: UIViewController {
+    
     var date: Date!
     var mood: MoodEntry.Mood!
+    var isEditingEntry = false
+    
+    private func updateUI() {
+        updateMood(to: mood)
+        datePicker.date = date
+    }
+    
+    private func updateMood(to newMood: MoodEntry.Mood) {
+        let unselectedColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        switch newMood {
+        case .none:
+            buttonAmazingMood.backgroundColor = unselectedColor
+            buttonGoodMood.backgroundColor = unselectedColor
+            buttonNeutralMood.backgroundColor = unselectedColor
+            buttonBadMood.backgroundColor = unselectedColor
+            buttonTerribleMood.backgroundColor = unselectedColor
+        case .amazing:
+            buttonAmazingMood.backgroundColor = newMood.colorValue
+            buttonGoodMood.backgroundColor = unselectedColor
+            buttonNeutralMood.backgroundColor = unselectedColor
+            buttonBadMood.backgroundColor = unselectedColor
+            buttonTerribleMood.backgroundColor = unselectedColor
+        case .good:
+            buttonAmazingMood.backgroundColor = unselectedColor
+            buttonGoodMood.backgroundColor = newMood.colorValue
+            buttonNeutralMood.backgroundColor = unselectedColor
+            buttonBadMood.backgroundColor = unselectedColor
+            buttonTerribleMood.backgroundColor = unselectedColor
+        case .neutral:
+            buttonAmazingMood.backgroundColor = unselectedColor
+            buttonGoodMood.backgroundColor = unselectedColor
+            buttonNeutralMood.backgroundColor = newMood.colorValue
+            buttonBadMood.backgroundColor = unselectedColor
+            buttonTerribleMood.backgroundColor = unselectedColor
+        case .bad:
+            buttonAmazingMood.backgroundColor = unselectedColor
+            buttonGoodMood.backgroundColor = unselectedColor
+            buttonNeutralMood.backgroundColor = unselectedColor
+            buttonBadMood.backgroundColor = newMood.colorValue
+            buttonTerribleMood.backgroundColor = unselectedColor
+        case .terrible:
+            buttonAmazingMood.backgroundColor = unselectedColor
+            buttonGoodMood.backgroundColor = unselectedColor
+            buttonNeutralMood.backgroundColor = unselectedColor
+            buttonBadMood.backgroundColor = unselectedColor
+            buttonTerribleMood.backgroundColor = newMood.colorValue
+        }
+        
+        mood = newMood
+    }
+    
+    @IBAction func pressSave(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func pressCancel(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBOutlet weak var buttonAmazingMood: UIButton!
+    @IBOutlet weak var buttonGoodMood: UIButton!
+    @IBOutlet weak var buttonNeutralMood: UIButton!
+    @IBOutlet weak var buttonBadMood: UIButton!
+    @IBOutlet weak var buttonTerribleMood: UIButton!
+    @IBAction func pressMood(_ button: UIButton) {
+        
+    }
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBAction func datePickerDidChangeValue(_ sender: UIDatePicker) {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(date, mood)
+        updateUI()
     }
 }
