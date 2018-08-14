99fa0f6b8104b2ea148d7ed80fbb2a7426d86a15
LinnierGames <esericksanc@gmail.com>
2018-08-14 12:47:31 -0700

    Populate the table view with dummy data by pressing a button in the app

--- Mood Tracker/ViewController.swift
+++ Mood Tracker/ViewController.swift
@@ -24,6 +24,13 @@ class ViewController: UIViewController {
         entries = [goodEntry, badEntry, neutralEntry]
         tableView.reloadData()
     }
+    
+    @IBAction func pressAddEntry(_ button: UIBarButtonItem) {
+        let now = Date()
+        let newMood = MoodEntry(mood: .amazing, date: now)
+        entries.insert(newMood, at: 0)
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+    }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
